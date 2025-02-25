d5b13f780ac43a23da6668332fcb18d6891670ef
nicolasgarea <nicogareacidre@gmail.com>
2025-02-25 20:37:23 +0100

    a

---
+++ src/main/java/wirtz/practicas/boletinhashmap/BoletinHashMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package wirtz.practicas.boletinhashmap;
+
+/**
+ *
+ * @author nicolas.gareacidre
+ */
+public class BoletinHashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
